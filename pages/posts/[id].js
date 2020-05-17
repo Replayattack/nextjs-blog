22e56eda96f7ce173601271c4f6bc64c906818a2
Daniel G <replayattack@pm.me>
2020-05-16 21:43:05 -0500

    Get markdown metadata from files and render in Post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from '../../components/layout'
-import {getAllPostIds} from '../../lib/posts'
+import {getAllPostIds, getPostData} from '../../lib/posts'
 
-export default function getStaticPaths() {
+export function getStaticPaths() {
   const paths = getAllPostIds()
 
   return {
@@ -10,6 +10,23 @@ export default function getStaticPaths() {
   }
 }
 
-export default function Post() {
-  return <Layout>...</Layout>
+export async function getStaticProps({params}) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
+
+export default function Post({postData}) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
 }
